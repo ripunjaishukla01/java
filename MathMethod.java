b0a610cfe66124921d3d7b02c0c319e5a52612e5
sripunjai <ripunjai79@gmail.com>
2025-04-25 16:50:47 +0530

    Math Class

---
+++ MathMethod.java
@@ -0,0 +1,17 @@
+class MathMethod{
+    public static void main(String args[]){
+        int x = 10;
+        int y = 20;
+        System.out.println(Math.max(x, y));
+        System.out.println(Math.min(x, y));
+        int num = 443293;
+        System.out.println(Math.sqrt(num));
+        int num1 = -893223;
+        System.out.println(Math.abs(num1));
+
+
+        System.out.println((int)Math.floor(Math.random()*100));
+        System.out.println(Math.round(Math.random()*100));
+        System.out.println(Math.pow(10, 3));
+    }
+}
