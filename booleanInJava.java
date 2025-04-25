57485e64fec356391ceac5968cfb5667829bd2e2
sripunjai <ripunjai79@gmail.com>
2025-04-25 17:10:44 +0530

    if else else if boolean

---
+++ booleanInJava.java
@@ -0,0 +1,12 @@
+public class booleanInJava {
+    public static void main(String[] args) {
+        boolean isPass = false;
+        System.out.println(isPass);
+
+        if(isPass){
+            System.out.println("Hey you win the mac Laptop");
+        }else{
+            System.out.println("You got thenga");
+        }
+    }
+}
---
+++ if_else_elseif.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class if_else_elseif {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age to check the eligibility to vote : ");
+        int age = sc.nextInt();
+        //If Condition
+        //if(cond){
+        // stattement;
+        //}
+        if(age>=18){
+            System.out.println("Yes you are eligible to vote");
+        }
+
+        //if(){
+        // statement 
+        //}
+        //else{
+        // statement
+        //}
+        if(age>=18){
+            System.out.println("you are eligible to vote");
+        }
+        else{
+            System.out.println("you are not eligible to vote");
+        }
+
+        ///if(cond)
+        /// {
+        /// statement
+        /// } elseif(cond){
+        /// statement 
+        /// }
+        /// else{
+        /// statement
+        /// }
+        
+        System.out.print("Enter your age to check whether you can drive or not : ");
+        int nAge = sc.nextInt();
+        if(nAge>18 && nAge <= 60){
+            System.out.println("You can drive");
+        }else if (nAge == 18) {
+            System.out.println("you can drive");
+        }else{
+            System.out.println("You can't drive");
+        }
+    }
+
+}
