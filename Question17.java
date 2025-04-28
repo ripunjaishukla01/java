ffdce10c814819ff4dbd05a66c900efedead7e64
sripunjai <ripunjai79@gmail.com>
2025-04-28 22:36:06 +0530

    Method Return Type

---
+++ Question17.java
@@ -0,0 +1,11 @@
+public class Question17 {
+    public static int square(int num) {
+        return num * num;
+    }
+
+    public static void main(String[] args) {
+        int result;
+        result = square(15);
+        System.out.println("Squared value of 10 is: " + result);
+    }
+}
--- switchCaseStatement.java
+++ switchCaseStatement.java
@@ -4,32 +4,33 @@ import java.util.Scanner;
 public class switchCaseStatement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a day number to know the day : " );
+        System.out.print("Enter a day number to know the day : ");
         int dayNum = sc.nextInt();
         switch (dayNum) {
-            case 1: 
-                System.out.println("Monday");
+            case 1:
+                System.out.println("Monday" + "Math");
                 break;
             case 2:
-                System.out.println("Tuesday");
+                System.out.println("Tuesday" + "Physics");
                 break;
-            case 3: 
-                System.out.println("Wednesday");
+            case 3:
+                System.out.println("Wednesday" + "English");
                 break;
             case 4:
-                System.out.println("Thursday");
+                System.out.println("Thursday" + "Hindi");
                 break;
-            case 5: 
-                System.out.println("Friday");
+            case 5:
+                System.out.println("Friday" + "Economics");
                 break;
             case 6:
-                System.out.println("Saturday");
+                System.out.println("Saturday" + "Dbms");
                 break;
             case 7:
-                System.out.println("Sunday");
-                break;             
+                System.out.println("Sunday" + "Chemistry");
+                break;
             default:
                 System.out.println("Not a day");
         }
+
     }
 }
