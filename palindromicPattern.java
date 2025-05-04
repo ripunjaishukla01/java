31a45bd3f6649d3d2f91c4bd257c65705cff5bc6
sripunjai <ripunjai79@gmail.com>
2025-05-04 23:28:26 +0530

    Palindromic Pattern Example

---
+++ palindromicPattern.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class palindromicPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows : ");
+        int rows = sc.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
