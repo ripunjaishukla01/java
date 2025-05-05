b366964ff18c281cf64b2b9738b5912ee5c97e55
sripunjai <ripunjai79@gmail.com>
2025-05-05 12:23:58 +0530

    New Commit

---
+++ Example3.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Example3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            System.out.println("Positive number");
+        } else if (number < 0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
