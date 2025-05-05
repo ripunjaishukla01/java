5d1cc189005ba51525066a2ec7be277ab076872d
sripunjai <ripunjai79@gmail.com>
2025-05-05 12:27:04 +0530

    New Commit

---
+++ Example4.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Example4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+
+        if (num1 > num2) {
+            System.out.println(num1 + " is the largest number.");
+        } else if (num2 > num1) {
+            System.out.println(num2 + " is the largest number.");
+        } else {
+            System.out.println("Both numbers are equal.");
+        }
+    }
+}
