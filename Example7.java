689a3ad97a8f358c3fc640205b93b33a81757da6
sripunjai <ripunjai79@gmail.com>
2025-05-06 23:41:50 +0530

    Boolean Question

---
+++ Example7.java
@@ -0,0 +1,11 @@
+public class Example7 {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        boolean c = a && b;
+        boolean d = a || b;
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
