a1831eff74214042bc047e7c34304b9c6dc6943d
sripunjai <ripunjai79@gmail.com>
2025-05-07 17:44:06 +0530

    Array

---
+++ Example9.java
@@ -0,0 +1,7 @@
+public class Example9 {
+    public static void main(String[] args) {
+        int[] marks = { 97, 98, 95 };
+        int[][] finalmarks = { { 97, 98, 96 }, { 95, 94, 94 }, { 93, 92, 92 } };
+        System.out.println(finalmarks[2][2]);
+    }
+}
