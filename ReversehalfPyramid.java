4a834cce9e8a6ea613ae30355dba74626ae9b8e1
sripunjai <ripunjai79@gmail.com>
2025-09-03 10:29:21 +0530

    Example

--- ReversehalfPyramid.java
+++ ReversehalfPyramid.java
@@ -1,20 +1,28 @@
 
 import java.util.Scanner;
 
-public class RightHalfPyramid {
+public class ReversehalfPyramid {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter number of rows: ");
         int rows = sc.nextInt();
-        for (int i = 1; i <= rows; i++) {
+
+        for (int i = rows; i >= 1; i--) {
 
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
+
             System.out.println();
         }
 
         sc.close();
+    }
+
+    @Override
+    public String toString() {
+        return "ReverseHalfPyramid []";
     }
 }
