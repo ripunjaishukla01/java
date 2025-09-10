0299b978a94d0a0e8e8becf896703b003ef5a357
sripunjai <ripunjai79@gmail.com>
2025-09-10 10:25:12 +0530

    Example

--- opps1.java
+++ opps1.java
@@ -1,30 +1,29 @@
-class pen {
+class opps {
     String color;
     String type;
 
     public void write() {
-        System.out.println("Writing somethings");
+        System.out.println("Writing something...");
     }
 
-    public void printcolor() {
+    public void printColor() {
         System.out.println(this.color);
     }
 }
 
 public class opps1 {
     public static void main(String[] args) {
-        pen pen1 = new pen();
-        pen1.color = "blue";
-        pen1.type = "gel";
+        opps pen1 = new opps();
+        pen1.color = "Blue";
+        pen1.type = "Gel";
 
-        pen pen2 = new pen();
-        pen2.color = "black";
-        pen2.type = "ballpoint";
+        opps pen2 = new opps();
+        pen2.color = "Black";
+        pen2.type = "Ballpoint";
 
-        pen1.printcolor();
-        pen2.printcolor();
+        pen1.printColor();
+        pen2.printColor();
 
-        // pen1.write();
-
+        pen1.write(); // optional usage
     }
 }
