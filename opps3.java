e27e617ad0afcfe6d6abfbec98d1de016649a5a5
sripunjai <ripunjai79@gmail.com>
2025-09-10 10:30:01 +0530

    Example

--- opps3.java
+++ opps3.java
@@ -1,26 +1,28 @@
-class Student {
+class Opps {
     String name;
     int age;
 
-    public void printinfo(String name) {
+    public void printInfo(String name) {
         System.out.println(name);
     }
 
-    public void printinfo(int age) {
+    public void printInfo(int age) {
         System.out.println(age);
     }
 
-    public void printInfo(String name1, int age) {
+    public void printInfo(String name, int age) {
         System.out.println(name + " " + age);
     }
 }
 
 public class opps3 {
     public static void main(String[] args) {
-        Student s1 = new Student();
+        Opps s1 = new Opps();
         s1.name = "Ripunjai";
         s1.age = 20;
+
+        s1.printInfo(s1.name);
+        s1.printInfo(s1.age);
         s1.printInfo(s1.name, s1.age);
     }
-
 }
