e7926a5382485fc18ede151756fe65df0a15d350
sripunjai <ripunjai79@gmail.com>
2025-10-07 20:22:15 +0530

    Example

---
+++ Example132.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Example132 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Number");
+        int sum = sc.nextInt();
+        System.out.println("Hello" + sum);
+
+        sc.close();
+    }
+}
