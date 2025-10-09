810735ac93a0a44534cc1ebd900d436b9ac2e481
sripunjai <ripunjai79@gmail.com>
2025-10-09 23:46:30 +0530

    ARRAY IMPORTANT

---
+++ Example133.java
@@ -0,0 +1,15 @@
+public class Example133 {
+    public static void main(String[] args) {
+        int[] arr = new int[7];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+        arr[5] = 60;
+        arr[6] = 70;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
