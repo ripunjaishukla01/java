4423e01c760a8b8f6ea6cf2e15775ec77b6088e6
sripunjai <ripunjai79@gmail.com>
2026-01-23 14:30:14 +0530

    example

---
+++ java 2026/GradeSystem.java
@@ -0,0 +1,19 @@
+
+public class GradeSystem {
+    public static String getGrade(int score) {
+        if (score >= 90 && score <= 100) {
+            return "A";
+        } else if (score >= 80 && score < 90) {
+            return "B";
+        } else if (score >= 70 && score < 80) {
+            return "C";
+        } else if (score >= 60 && score < 70) {
+            return "D";
+        } else if (score >= 0 && score < 60) {
+            return "F";
+        } else {
+            return "Invalid score";
+        }
+    }
+
+}
