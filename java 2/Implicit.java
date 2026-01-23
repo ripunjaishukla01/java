ebcdb02b79b42ecfc8462d02c2ebb18e76349e90
sripunjai <ripunjai79@gmail.com>
2026-01-23 14:27:14 +0530

    example

---
+++ java 2/Implicit.java
@@ -0,0 +1,14 @@
+class Implicit {
+    public static void main(String args[]) {
+        int myNum = 100000;
+        System.out.println(myNum);
+        
+        int mynum1 = 12;
+        System.out.println(mynum1);
+
+        long myNum = 1000L;
+        System.out.println(myNum);
+
+
+    }
+}
---
+++ java 2/student.java
@@ -0,0 +1,16 @@
+public class student {
+    public static void main(String[] args) {
+        String studentName = "John Doe";
+int studentID = 900;
+int studentAge = 119;
+float studentFee = 190.25f;
+char studentGrade = 'A';
+
+// Print variables
+System.out.println("Student name: " + studentName);
+System.out.println("Student id: " + studentID);
+System.out.println("Student age: " + studentAge);
+System.out.println("Student fee: " + studentFee);
+System.out.println("Student grade: " + studentGrade);
+    }
+}
