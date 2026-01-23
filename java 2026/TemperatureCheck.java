49f9cce2ce48cb1bc8dee0ad1d83bf66f5e800cb
sripunjai <ripunjai79@gmail.com>
2026-01-23 14:34:01 +0530

    example

---
+++ java 2026/TemperatureCheck.java
@@ -0,0 +1,24 @@
+
+    import java.util.Scanner;
+
+public class TemperatureCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter temperature: ");
+        int temp = sc.nextInt();
+
+        if (temp < 15) {
+            System.out.println("Cold");
+        } else if (temp >= 15 && temp <= 30) {
+            System.out.println("Normal");
+        } else {
+            System.out.println("Hot");
+        }
+
+        sc.close();
+    }
+}
+
+    
+
