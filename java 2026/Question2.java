02d5d17da9ab99aadae8e28a2ffa10d7be7db42b
sripunjai <ripunjai79@gmail.com>
2026-01-26 11:31:41 +0530

    Example

---
+++ java 2026/Question2.java
@@ -0,0 +1,25 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n - i - 1];
+            arr[n - i - 1] = temp;
+        }
+        System.out.println("Reversed array:");
+        System.out.println(Arrays.toString(arr));
+
+        sc.close();
+    }
+}
