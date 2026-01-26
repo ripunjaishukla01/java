3cfd6a59f825033480d4662c018e1d22f2fa51be
sripunjai <ripunjai79@gmail.com>
2026-01-26 11:31:03 +0530

    Example

---
+++ Question66.java
@@ -0,0 +1,6 @@
+public class Question66 {
+    public static void main(String[] args) {
+        int[] marks = { 85, 90, 78, 92, 88, 76, 95, 89, 84, 91 };
+        System.out.println(marks.length);
+    }
+}
