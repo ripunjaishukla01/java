9c10d66300fa5e42bba3f423f988c08904a9fff2
sripunjai <ripunjai79@gmail.com>
2026-01-27 14:50:39 +0530

    Example

---
+++ java 2026/RS.java
@@ -0,0 +1,11 @@
+public class RS {
+    public static void main(String[] args) {
+    int [] marks = {45, 67, 89, 90, 23};
+     int sum = 0;
+     for(int element: marks){
+        sum = sum + element;
+     } 
+        System.out.println("The sum is: " + sum);
+     
+    }
+}
